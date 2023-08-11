86d0cb61b8222a778f9f61753dc039cc35a67957
smart-life-tech <christlightworld@gmail.com>
2023-08-11 15:20:09 +0100

    modified:   Core/Src/main.cpp

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -46,25 +46,8 @@ void microDelay(uint16_t delay)
     ;
 }
 
-void step(int steps, uint8_t direction, uint16_t delay)
-{
-  if (direction == 0)
-    HAL_GPIO_WritePin(DIR_PORT, DIR_PIN, GPIO_PIN_SET);
-  else
-    HAL_GPIO_WritePin(DIR_PORT, DIR_PIN, GPIO_PIN_RESET);
-  for (x = 0; x < steps; x = x + 1)
-  {
-    std::string displayStr = "steps: " + std::to_string(x);
-    DISPLAY.SSD1306_GotoXY(0, 0);
-    DISPLAY.SSD1306_UpdateScreen();
-    DISPLAY.SSD1306_Puts(const_cast<char *>(displayStr.c_str()), &Font_11x18, 0x01);
-    DISPLAY.SSD1306_UpdateScreen();
-    HAL_GPIO_WritePin(STEP_PORT, STEP_PIN, GPIO_PIN_SET);
-    microDelay(delay);
-    HAL_GPIO_WritePin(STEP_PORT, STEP_PIN, GPIO_PIN_RESET);
-    microDelay(delay);
-  }
-}
+
+
 /* USER CODE END 0 */
 
 /**
@@ -328,6 +311,25 @@ void Error_Handler(void)
   {
   }
   /* USER CODE END Error_Handler_Debug */
+}
+void step(int steps, uint8_t direction, uint16_t delay)
+{
+  if (direction == 0)
+    HAL_GPIO_WritePin(DIR_PORT, DIR_PIN, GPIO_PIN_SET);
+  else
+    HAL_GPIO_WritePin(DIR_PORT, DIR_PIN, GPIO_PIN_RESET);
+  for (x = 0; x < steps; x = x + 1)
+  {
+    std::string displayStr = "steps: " + std::to_string(x);
+    DISPLAY.SSD1306_GotoXY(0, 0);
+    DISPLAY.SSD1306_UpdateScreen();
+    DISPLAY.SSD1306_Puts(const_cast<char *>(displayStr.c_str()), &Font_11x18, 0x01);
+    DISPLAY.SSD1306_UpdateScreen();
+    HAL_GPIO_WritePin(STEP_PORT, STEP_PIN, GPIO_PIN_SET);
+    microDelay(delay);
+    HAL_GPIO_WritePin(STEP_PORT, STEP_PIN, GPIO_PIN_RESET);
+    microDelay(delay);
+  }
 }
 int main(void)
 {
